bc19ec92dee0106b0af622be52205f6b1bc77cd5
Mohamed Ehab <104400937+ehab1441@users.noreply.github.com>
2024-10-28 05:20:37 +0300

    Recommendation_System.py

--- Recommendation_System.py
+++ Recommendation_System.py
@@ -9,7 +9,7 @@ import random
 import re
 
 # Load the movies data (correcting the path)
-movies = pd.read_csv(r"C:\Users\mehab\OneDrive\Desktop\movies_data.csv", lineterminator='\n')
+movies = pd.read_csv(r"movies_data.csv", lineterminator='\n')
 
 def extract_years(df):
     df['Release_Year'] = pd.to_datetime(df['Release_Year'], errors='coerce').dt.year
