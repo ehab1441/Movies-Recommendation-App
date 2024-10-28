fa3d3b693b48e7ef3c8d8ac2e11da890492a19fd
Mohamed Ehab <104400937+ehab1441@users.noreply.github.com>
2024-10-28 05:54:08 +0300

    app.py

--- app.py
+++ app.py
@@ -69,3 +69,7 @@ if st.button("Recommend a movie"):
             st.write(f"Average Rating: {movie_info['Vote_Average']} ⭐")
             st.write(f"Release Year: {movie_info['Release_Year']}")
             st.write(f"Genre: {movie_info['Genre']}")
+            
+        else:
+            st.write("No movies found matching the criteria.")
+
